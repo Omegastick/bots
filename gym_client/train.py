f5af5ae4aa068ae86a6dca4133590ec57aee130d
Omegastick <Omegastick@hotmail.co.uk>
2018-10-25 22:29:32 +0900

    Removed clipped rewards

--- gym_client/train.py
+++ gym_client/train.py
@@ -77,7 +77,6 @@ class Trainer:
 
             # Step environments
             observations, rewards, dones, _ = self.env.step(actions)
-            rewards = np.clip(rewards, -1, 1)
 
             # Give rewards
             for i, reward in enumerate(rewards):
