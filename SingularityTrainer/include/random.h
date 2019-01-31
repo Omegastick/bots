452a148a2762f92cef73cd1fc97eefb11ebbd5a0
Omegastick <omegastick@hotmail.co.uk>
2019-01-31 19:26:05 +0900

    Revert "Revert "Add basic particle system""
    This reverts commit 63316d7b224a01c5ac24724d8fd383a6374f9139.

--- SingularityTrainer/include/random.h
+++ SingularityTrainer/include/random.h
@@ -10,8 +10,9 @@ class Random
     Random(int seed);
     ~Random();
 
-    int NextInt(int min, int max);
-    float NextFloat(float min, float max);
+    int next_int(int min, int max);
+    float next_float(float min, float max);
+    float next_float(std::uniform_real_distribution<float> distribution);
 
   private:
     std::mt19937 rng;
---
+++ cpp/include/iparticle_system.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "idrawable.h"
+
+namespace SingularityTrainer
+{
+class IParticleSystem : public IDrawable
+{
+  public:
+    IParticleSystem(){};
+    virtual ~IParticleSystem() = 0;
+
+    virtual void update(float delta_time) = 0;
+    virtual void draw(sf::RenderTarget &render_target) = 0;
+
+  private:
+    IParticleSystem &operator=(const IParticleSystem &) = default;
+};
+
+inline IParticleSystem::~IParticleSystem() {}
+}
