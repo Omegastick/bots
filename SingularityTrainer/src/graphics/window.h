683b1ad098d1125935a4f456b36f429c95a25df1
Omegastick <omegastick@hotmail.co.uk>
2019-02-08 16:17:12 +0900

    Abstract window to own class

--- SingularityTrainer/src/graphics/window.h
+++ SingularityTrainer/src/graphics/window.h
@@ -1,13 +1,21 @@
+#pragma once
+
 #include <iostream>
+#include <string>
 
 #include "GLFW/glfw3.h"
-#include "glad/glad.h"
 
 namespace SingularityTrainer
 {
 class Window
 {
   public:
-    Window();
+    Window(int x, int y, std::string title, int major_opengl_version, int minor_opengl_version);
+    ~Window();
+
+    void swap_buffers();
+    bool should_close();
+
+    GLFWwindow *window;
 };
 }
