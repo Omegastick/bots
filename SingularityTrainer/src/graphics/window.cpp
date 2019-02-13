1055164bb2b3cafe29129af6b86d51c04dd59994
Omegastick <omegastick@hotmail.co.uk>
2019-02-13 15:44:12 +0900

    Enable multisampling

--- SingularityTrainer/src/graphics/window.cpp
+++ SingularityTrainer/src/graphics/window.cpp
@@ -108,6 +108,7 @@ Window::Window(int x, int y, std::string title, int opengl_major_version, int op
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, opengl_minor_version);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     window = glfwCreateWindow(x, y, title.c_str(), nullptr, nullptr);
     if (!window)
@@ -139,6 +140,7 @@ Window::Window(int x, int y, std::string title, int opengl_major_version, int op
     }
 
     glEnable(GL_BLEND);
+    glEnable(GL_MULTISAMPLE);
 
     spdlog::debug("Actual OpenGL version: {}", glGetString(GL_VERSION));
 }
