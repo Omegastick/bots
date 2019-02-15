dcff17c49b97debe6b81a283481228a09dca2741
Omegastick <omegastick@hotmail.co.uk>
2019-02-15 18:15:05 +0900

    Add particle colours

--- SingularityTrainer/src/graphics/screens/particle_test_screen.cpp
+++ SingularityTrainer/src/graphics/screens/particle_test_screen.cpp
@@ -35,6 +35,9 @@ ParticleTestScreen::ParticleTestScreen(
     particle_positions.resize(max_particles);
     particle_velocities.resize(max_particles);
     particle_start_times.resize(max_particles);
+    particle_lives.resize(max_particles);
+    particle_start_colors.resize(max_particles);
+    particle_end_colors.resize(max_particles);
 
     this->resource_manager = &resource_manager;
     resource_manager.load_shader("particle", "shaders/particle.vert", "shaders/default.frag");
@@ -48,6 +51,9 @@ ParticleTestScreen::ParticleTestScreen(
     position_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(glm::vec2), GL_STREAM_DRAW);
     velocity_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(glm::vec2), GL_STREAM_DRAW);
     start_time_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(float), GL_STREAM_DRAW);
+    life_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(float), GL_STREAM_DRAW);
+    start_color_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(glm::vec4), GL_STREAM_DRAW);
+    end_color_vertex_buffer = std::make_unique<VertexBuffer>(nullptr, max_particles * sizeof(glm::vec4), GL_STREAM_DRAW);
 
     // Set attribute pointer for quad
     vertex_array.bind();
@@ -73,6 +79,24 @@ ParticleTestScreen::ParticleTestScreen(
     glEnableVertexAttribArray(3);
     glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, 0, (void *)0);
     glVertexAttribDivisor(3, 1);
+
+    // Set attribute pointer for lives
+    life_vertex_buffer->bind();
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, 0, (void *)0);
+    glVertexAttribDivisor(4, 1);
+
+    // Set attribute pointer for start colors
+    start_color_vertex_buffer->bind();
+    glEnableVertexAttribArray(5);
+    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
+    glVertexAttribDivisor(5, 1);
+
+    // Set attribute pointer for end colors
+    end_color_vertex_buffer->bind();
+    glEnableVertexAttribArray(6);
+    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
+    glVertexAttribDivisor(6, 1);
 }
 
 ParticleTestScreen::~ParticleTestScreen() {}
@@ -85,8 +109,11 @@ void ParticleTestScreen::update(const float delta_time)
     for (int i = 0; i < 5; ++i)
     {
         particle_positions[current_particle_index] = glm::vec2(960, 540);
-        particle_start_times[current_particle_index] = time + (float)i / 600;
         particle_velocities[current_particle_index] = glm::diskRand<float>(500);
+        particle_start_times[current_particle_index] = time + (float)i / 600;
+        particle_lives[current_particle_index] = 5;
+        particle_start_colors[current_particle_index] = glm::vec4(0.5, 1.0, 1.0, 1.0);
+        particle_end_colors[current_particle_index] = glm::vec4(1.0, 0.5, 1.0, 0.0);
         current_particle_index++;
         current_particle_index %= max_particles;
         particle_count = particle_count > current_particle_index ? particle_count : current_particle_index;
@@ -109,6 +136,12 @@ void ParticleTestScreen::draw(bool lightweight)
     velocity_vertex_buffer->add_sub_data(&particle_velocities[0], 0, particle_count * sizeof(glm::vec2));
     start_time_vertex_buffer->clear();
     start_time_vertex_buffer->add_sub_data(&particle_start_times[0], 0, particle_count * sizeof(float));
+    life_vertex_buffer->clear();
+    life_vertex_buffer->add_sub_data(&particle_lives[0], 0, particle_count * sizeof(float));
+    start_color_vertex_buffer->clear();
+    start_color_vertex_buffer->add_sub_data(&particle_start_colors[0], 0, particle_count * sizeof(glm::vec4));
+    end_color_vertex_buffer->clear();
+    end_color_vertex_buffer->add_sub_data(&particle_end_colors[0], 0, particle_count * sizeof(glm::vec4));
 
     glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, particle_count);
 
