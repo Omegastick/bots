3712b916689e3c40b370eecc3ba108d6115a47f9
Omegastick <omegastick@hotmail.co.uk>
2019-02-26 13:38:30 +0900

    Add CRT effect to TargetEnvScreen

--- SingularityTrainer/src/screens/target_env_screen.cpp
+++ SingularityTrainer/src/screens/target_env_screen.cpp
@@ -15,7 +15,7 @@
 namespace SingularityTrainer
 {
 TargetEnvScreen::TargetEnvScreen(ResourceManager &resource_manager, Communicator *communicator, Random *rng, int env_count)
-    : lightweight_rendering(false), projection(glm::ortho(0.f, 1920.f, 0.f, 1080.f)), fast(false)
+    : lightweight_rendering(false), projection(glm::ortho(0.f, 1920.f, 0.f, 1080.f)), fast(false), resource_manager(&resource_manager)
 {
     trainer = std::make_unique<QuickTrainer>(resource_manager, communicator, rng, env_count);
 
@@ -26,14 +26,9 @@ TargetEnvScreen::TargetEnvScreen(ResourceManager &resource_manager, Communicator
     resource_manager.load_texture("bullet", "images/bullet.png");
     resource_manager.load_texture("target", "images/target.png");
     resource_manager.load_texture("pixel", "images/pixel.png");
+    resource_manager.load_shader("crt", "shaders/texture.vert", "shaders/crt.frag");
 
-    // resource_manager.load_shader("crt", "shaders/crt.frag");
-    // shader = resource_manager.shader_store.get("crt");
-    // shader->setUniform("texture", sf::Shader::CurrentTexture);
-    // shader->setUniform("resolution", sf::Vector2f(1920, 1080));
-    // shader->setUniform("output_gamma", 1.1f);
-    // shader->setUniform("strength", 0.3f);
-    // shader->setUniform("distortion_factor", 0.08f);
+    crt_post_proc_layer = std::make_unique<PostProcLayer>(resource_manager.shader_store.get("crt").get());
 
     trainer->begin_training();
 }
@@ -62,11 +57,18 @@ void TargetEnvScreen::update(const float delta_time)
 
 void TargetEnvScreen::draw(Renderer &renderer, bool lightweight)
 {
+    renderer.push_post_proc_layer(crt_post_proc_layer.get());
     renderer.begin();
 
     renderer.scissor(-10, -10, 10, 10, glm::ortho(-19.2f, 19.2f, -10.8f, 10.8f));
     auto render_data = trainer->environments[0]->get_render_data(lightweight_rendering);
     renderer.draw(render_data, glm::ortho(-19.2f, 19.2f, -10.8f, 10.8f), trainer->environments[0]->get_elapsed_time(), lightweight_rendering);
+
+    auto crt_shader = resource_manager->shader_store.get("crt");
+    crt_shader->set_uniform_2f("u_resolution", glm::vec2(renderer.get_width(), renderer.get_height()));
+    crt_shader->set_uniform_1f("u_output_gamma", 1);
+    crt_shader->set_uniform_1f("u_strength", 0.5);
+    crt_shader->set_uniform_1f("u_distortion_factor", 0.03);
 
     renderer.end();
 }
