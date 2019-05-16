cc5ddbb36efbc4227a9076bc54a8e012d92eddb5
Omegastick <omegastick@hotmail.co.uk>
2019-05-16 21:02:21 +0900

    Add basic part selector

--- src/screens/build_screen.cpp
+++ src/screens/build_screen.cpp
@@ -1,8 +1,11 @@
 #include <memory>
 
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <imgui.h>
 
 #include "graphics/renderers/renderer.h"
+#include "graphics/sprite.h"
 #include "screens/build_screen.h"
 #include "resource_manager.h"
 #include "screen_manager.h"
@@ -13,18 +16,44 @@ namespace SingularityTrainer
 BuildScreen::BuildScreen(ResourceManager &resource_manager, ScreenManager &screen_manager)
     : resource_manager(&resource_manager),
       screen_manager(&screen_manager),
-      part_selector_window(std::make_unique<PartSelectorWindow>())
+      part_selector_window(std::make_unique<PartSelectorWindow>(resource_manager)),
+      available_parts({"base_module", "gun_module", "thruster_module", "laser_sensor_module"}),
+      selected_part(),
+      ghost(std::make_unique<Sprite>("")),
+      projection(glm::ortho(0.f, 1920.f, 0.f, 1080.f))
 {
+    resource_manager.load_texture("base_module", "images/base_module.png");
+    for (const auto &part : available_parts)
+    {
+        resource_manager.load_texture(part, "images/" + part + ".png");
+    }
+    resource_manager.load_shader("texture", "shaders/texture.vert", "shaders/texture.frag");
 }
 
 void BuildScreen::update(double /*delta_time*/)
 {
-    part_selector_window->update();
+    auto selected_part_ = part_selector_window->update(available_parts);
+    if (selected_part_ != "")
+    {
+        selected_part = selected_part_;
+        ghost->set_texture(selected_part);
+        ghost->set_scale(glm::vec2(100, 100));
+        ghost->set_origin(ghost->get_center());
+        ghost->set_color(cl_white);
+    }
 }
 
 void BuildScreen::draw(Renderer &renderer, bool /*lightweight*/)
 {
     renderer.begin();
+
+    if (selected_part != "")
+    {
+        auto cursor_pos = ImGui::GetCursorPos();
+        ghost->set_position({cursor_pos.x, cursor_pos.y});
+        renderer.draw(*ghost, projection);
+    }
+
     renderer.end();
 }
 }
--- src/ui/build_screen/part_selector_window.cpp
+++ src/ui/build_screen/part_selector_window.cpp
@@ -1,14 +1,51 @@
-#include <imgui.h>
+#pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
 
+#include <string>
+#include <vector>
+
+#include <imgui.h>
+#include <spdlog/spdlog.h>
+
+#include "graphics/backend/texture.h"
+#include "graphics/colors.h"
+#include "resource_manager.h"
 #include "ui/build_screen/part_selector_window.h"
+#include "utilities.h"
 
 namespace SingularityTrainer
 {
-PartSelectorWindow::PartSelectorWindow() {}
+PartSelectorWindow::PartSelectorWindow(ResourceManager &resource_manager)
+    : resource_manager(&resource_manager)
+{
+}
 
-void PartSelectorWindow::update() {
+std::string PartSelectorWindow::update(std::vector<std::string> &parts)
+{
+    std::string selected_part = "";
+    ImGui::PushStyleColor(ImGuiCol_Button, glm_to_im(cl_base03));
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, glm_to_im(cl_base03));
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, glm_to_im(cl_base02));
+    ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 5);
+    ImGui::SetNextWindowSize({400, 600}, ImGuiCond_FirstUseEver);
     ImGui::Begin("Part Selector");
-    ImGui::Text("I am the part selector");
+    auto style = ImGui::GetStyle();
+    auto image_size = ((ImGui::GetContentRegionAvailWidth() - style.ScrollbarSize * 2) / 3) - style.ItemSpacing.x;
+    for (unsigned int i = 0; i < parts.size(); ++i)
+    {
+        if (i % 3 != 0)
+        {
+            ImGui::SameLine();
+        }
+        auto texture = resource_manager->texture_store.get(parts[i]);
+        if (ImGui::ImageButton(ImTextureID(texture->get_id()), ImVec2(image_size, image_size), {1, 1}, {0, 0}))
+        {
+            selected_part = parts[i];
+        }
+    }
     ImGui::End();
+    ImGui::PopStyleVar();
+    ImGui::PopStyleColor(3);
+
+    return selected_part;
 }
 }
