15cf4710e370fb286a477498b2a840804de47816
Omegastick <omegastick@hotmail.co.uk>
2019-05-20 17:02:07 +0900

    Implement ShipBuilder::get_module_at_point()

--- src/ui/build_screen/ship_builder.cpp
+++ src/ui/build_screen/ship_builder.cpp
@@ -2,26 +2,64 @@
 
 #include <Box2D/Box2D.h>
 #include <doctest/doctest.h>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/glm.hpp>
 #include <nlohmann/json.hpp>
 
 #include "ui/build_screen/ship_builder.h"
+#include "io.h"
 #include "random.h"
 #include "training/agents/agent.h"
 #include "training/modules/base_module.h"
 #include "training/modules/module_link.h"
+#include "training/rigid_body.h"
 
 namespace SingularityTrainer
 {
-ShipBuilder::ShipBuilder(b2World &b2_world, Random &rng)
-    : agent(std::make_unique<Agent>(b2_world, &rng))
+bool GetAllQueryCallback::ReportFixture(b2Fixture *fixture)
+{
+    fixtures.push_back(fixture);
+    return true;
+}
+
+ShipBuilder::ShipBuilder(b2World &b2_world, Random &rng, IO &io)
+    : agent(std::make_unique<Agent>(b2_world, &rng)),
+      io(&io),
+      b2_world(&b2_world),
+      projection(glm::ortho(-9.6f, 9.6f, -5.4f, 5.4f))
 {
     auto base_module = std::make_shared<BaseModule>();
     agent->add_module(base_module);
     agent->update_body();
 }
 
-IModule *ShipBuilder::get_module_at_point(glm::vec2 /*point*/) { return agent->get_modules()[0].get(); }
+IModule *ShipBuilder::get_module_at_point(glm::vec2 point)
+{
+    auto resolution = static_cast<glm::vec2>(io->get_resolution());
+    auto centered_point = point - (resolution / 2.f);
+    auto normalized_point = centered_point / resolution;
+    auto world_point = glm::vec2(normalized_point.x * 9.6, normalized_point.y * 5.4);
+
+    GetAllQueryCallback query_callback;
+    b2_world->QueryAABB(&query_callback, {{world_point.x, world_point.y},
+                                          {world_point.x, world_point.y}});
+
+    auto collisions = query_callback.get_collisions();
+    if (collisions.size() == 0)
+    {
+        return nullptr;
+    }
+
+    auto fixture = collisions[0];
+    auto rigid_body = static_cast<RigidBody *>(fixture->GetBody()->GetUserData());
+    if (rigid_body->parent_type != RigidBody::ParentTypes::Agent)
+    {
+        return nullptr;
+    }
+
+    return static_cast<IModule *>(fixture->GetUserData());
+}
+
 ModuleLinkAndDistance ShipBuilder::get_nearest_module_link(glm::vec2 /*point*/) { return {}; }
 void ShipBuilder::update() {}
 
@@ -29,7 +67,9 @@ TEST_CASE("ShipBuilder")
 {
     b2World b2_world({0, 0});
     Random rng(0);
-    ShipBuilder ship_builder(b2_world, rng);
+    IO io;
+    io.set_resolution(1920, 1080);
+    ShipBuilder ship_builder(b2_world, rng, io);
 
     SUBCASE("Starts with only a single base module")
     {
@@ -50,7 +90,7 @@ TEST_CASE("ShipBuilder")
     {
         SUBCASE("Correctly selects the base module at 0, 0")
         {
-            auto selected_module = ship_builder.get_module_at_point({0, 0});
+            auto selected_module = ship_builder.get_module_at_point({960, 540});
             auto base_module = ship_builder.get_agent()->get_modules()[0].get();
 
             CHECK(selected_module == base_module);
--- src/ui/build_screen/ship_builder.h
+++ src/ui/build_screen/ship_builder.h
@@ -1,15 +1,27 @@
 #include <memory>
+#include <vector>
 
-#include <glm/vec2.hpp>
-
-class b2World;
+#include <Box2D/Box2D.h>
+#include <glm/glm.hpp>
 
 namespace SingularityTrainer
 {
 class Agent;
 class IModule;
+class IO;
 class ModuleLink;
 class Random;
+
+class GetAllQueryCallback : public b2QueryCallback
+{
+  private:
+    std::vector<b2Fixture *> fixtures;
+
+  public:
+    virtual bool ReportFixture(b2Fixture *fixture);
+
+    inline std::vector<b2Fixture *> &get_collisions() { return fixtures; }
+};
 
 struct ModuleLinkAndDistance
 {
@@ -21,9 +33,12 @@ class ShipBuilder
 {
   private:
     std::unique_ptr<Agent> agent;
+    IO *io;
+    b2World *b2_world;
+    glm::mat4 projection;
 
   public:
-    ShipBuilder(b2World &b2_world, Random &rng);
+    ShipBuilder(b2World &b2_world, Random &rng, IO &io);
 
     IModule *get_module_at_point(glm::vec2 point);
     ModuleLinkAndDistance get_nearest_module_link(glm::vec2 point);
