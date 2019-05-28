120946f13c5121a5642088c3c6d916d0553fd454
Omegastick <omegastick@hotmail.co.uk>
2019-05-28 19:53:31 +0900

    Clean up the graphics playground

---
+++ src/graphics/screens/screens.h
@@ -0,0 +1,8 @@
+#include "graphics/screens/quad_screen.h"
+#include "graphics/screens/texture_test_screen.h"
+#include "graphics/screens/sprite_test_screen.h"
+#include "graphics/screens/post_proc_screen.h"
+#include "graphics/screens/particle_test_screen.h"
+#include "graphics/screens/line_test_screen.h"
+#include "graphics/screens/crt_test_screen.h"
+#include "graphics/screens/text_test_screen.h"
