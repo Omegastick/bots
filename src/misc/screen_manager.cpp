285265a58204e12c19fb5ca30246043f378cff5f
Omegastick <omegastick@hotmail.co.uk>
2019-05-30 17:27:06 +0900

    Add option to start from scratch in wizard

--- src/misc/screen_manager.cpp
+++ src/misc/screen_manager.cpp
@@ -14,10 +14,20 @@ void ScreenManager::show_screen(std::shared_ptr<IScreen> screen)
 {
     screens.push(screen);
 }
+
 void ScreenManager::close_screen()
 {
     screens_to_pop += 1;
 }
+
+void ScreenManager::exit()
+{
+    while (stack_size() > 0)
+    {
+        screens.pop();
+    }
+}
+
 void ScreenManager::update(double delta_time)
 {
     for (int i = 0; i < screens_to_pop; ++i)
@@ -28,10 +38,12 @@ void ScreenManager::update(double delta_time)
 
     screens.top()->update(delta_time);
 }
+
 int ScreenManager::stack_size()
 {
     return screens.size();
 }
+
 void ScreenManager::draw(Renderer &renderer, bool lightweight)
 {
     screens.top()->draw(renderer, lightweight);
