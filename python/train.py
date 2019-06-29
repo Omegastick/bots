5b9db01ad4fbc84d92e7d5571c4a49b9a0c11114
omegastick <omegastick@hotmail.co.uk>
2019-06-29 01:45:03 +0000

    Add headless training script

---
+++ python/train.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from pathlib import Path
+import singularity_trainer
+import time
+
+PROGRAM_FILE = '../programs/asd.json'
+
+
+def main():
+    # If anything is logged during imports, it messes up our logging so we
+    # reset the logging module here
+    root_logger = logging.getLogger()
+    if root_logger.handlers:
+        for handler in root_logger.handlers:
+            root_logger.removeHandler(handler)
+    logging.basicConfig(level=logging.DEBUG,
+                        format=('%(asctime)s %(funcName)s '
+                                '[%(levelname)s]: %(message)s'),
+                        datefmt='%Y%m%d %H:%M:%S')
+
+    with open(PROGRAM_FILE, 'r') as file:
+        trainer = singularity_trainer.make_trainer(file.read())
+
+    try:
+        last_test_time = time.time()
+        test_number = 1
+        while True:
+            trainer.step()
+            if time.time() - last_test_time > 600:
+                logging.info("######## Testing ########")
+                winrate = trainer.evaluate(1000)
+                logging.info("Result: %f", winrate)
+                logging.info("######## Tested  ########")
+                last_test_time = time.time()
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == '__main__':
+    main()
