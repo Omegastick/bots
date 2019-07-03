5b2109b33ec89c59b0676b3dbcf4d558ee1ae074
Omegastick <omegastick@hotmail.co.uk>
2019-07-03 18:21:48 +0900

    Allow for selecting a checkpoint which making a program

--- src/ui/create_program_screen/brain_window.h
+++ src/ui/create_program_screen/brain_window.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <filesystem>
 #include <memory>
 
 namespace SingularityTrainer
@@ -11,7 +12,11 @@ class TrainingProgram;
 class BrainWindow
 {
   private:
+    Checkpointer &checkpointer;
+    std::vector<std::filesystem::path> checkpoints;
     IO &io;
+    int last_selected_file;
+    int selected_file;
 
   public:
     BrainWindow(Checkpointer &checkpointer, IO &io);
