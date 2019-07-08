12db4ad1e500861f83fdda655a8d6930c8356954
Omegastick <omegastick@hotmail.co.uk>
2019-07-08 11:39:35 +0900

    Add logging to train.py

--- python/train.py
+++ python/train.py
@@ -1,9 +1,11 @@
-import os
+from datetime import datetime
 import logging
+import os
 from pathlib import Path
 import sys
-import singularity_trainer
 import time
+import singularity_trainer
+import tensorflow as tf
 
 
 def main():
@@ -21,15 +23,22 @@ def main():
     with open(sys.argv[1], 'r') as file:
         trainer = singularity_trainer.make_trainer(file.read())
 
+    log_dir = os.path.join(
+        "runs", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    writer = tf.summary.create_file_writer(log_dir)
+
     try:
         last_test_time = time.time()
-        test_number = 1
+        test_number = 0
         while True:
             trainer.step()
-            if time.time() - last_test_time > 1:
+            if time.time() - last_test_time > 600:
+                test_number += 1
                 logging.info("######## Testing ########")
                 elo = trainer.evaluate()
                 logging.info("Result: %f", elo)
+                with writer.as_default():
+                    tf.summary.scalar("elo", elo, step=test_number)
                 logging.info("######## Tested  ########")
                 last_test_time = time.time()
     except KeyboardInterrupt:
