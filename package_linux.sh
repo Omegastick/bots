b8a1bcb9a001dc3f6ddb0243f2fda1cef0983d70
Omegastick <omegastick@hotmail.co.uk>
2019-09-04 14:49:27 +0900

    Add Linux packaging script (#86)

---
+++ package_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+function cleanup {
+    cd ..
+    rm -r packaging
+}
+trap cleanup EXIT
+
+VERSION=0.0.0
+FILENAME="singularity-trainer-${VERSION}-linux.tar.gz"
+
+
+# Set up temp directory
+mkdir -p packaging
+cd packaging || exit
+
+# Copy executable
+cp ../build/SingularityTrainer .
+
+# Copy assets
+cp -r ../assets .
+
+# Copy dependencies
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.2.0.zip 
+unzip libtorch-cxx11-abi-shared-with-deps-1.2.0.zip libtorch/lib/*
+mv libtorch/lib/*.so* .
+rm -rf libtorch/
+rm libtorch-cxx11-abi-shared-with-deps-1.2.0.zip
+
+# Create archive
+tar -czvf "${FILENAME}" ./*
+mv "${FILENAME}" ..
