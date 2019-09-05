654d00439bb24a8cec6e59854d242bb54b650cc1
Omegastick <omegastick@hotmail.co.uk>
2019-09-05 17:41:27 +0900

    Add cloud login (#95)

---
+++ st-cloud/main.py
@@ -0,0 +1,46 @@
+"""
+Serverless functions for the Singularity Trainer matchmaking server.
+"""
+
+import secrets
+
+from flask import jsonify
+from google.cloud import firestore
+
+
+def login(request):
+    """
+    Log a user into the matchmaking system, returning a session token.
+    If the user does not exist, creates one.
+    Raises an exception if multiple users with the same username exist.
+    """
+    # Init database
+    db = firestore.Client()
+
+    username = request.json['username']
+
+    users = db.collection('users')
+
+    # Get all users matching the requested username
+    matching_users_query = users.where('username', '==', username)
+    matching_users = [x for x in matching_users_query.stream()]
+    user: firestore.DocumentReference = None
+
+    if len(matching_users) == 1:
+        # User exists
+        user = users.document(matching_users[0].id)
+    elif len(matching_users) > 1:
+        # Multiple users with the same username exist
+        # This shouldn't happen
+        matching_users_dicts = [x.to_dict() for x in matching_users]
+        raise Exception(f"{len(matching_users)} matching users in database: "
+                        f"{matching_users_dicts}")
+    else:
+        # Create a new user
+        user = users.add({'username': username, 'elo': 0})[1]
+
+    token = secrets.token_urlsafe()
+    user.update({'token': token,
+                 'token_set_time': firestore.SERVER_TIMESTAMP})
+
+    return jsonify({'token': token})
