d757c5d0afa4ceb31dc56edae7ee705883a6434e
Omegastick <omegastick@hotmail.co.uk>
2019-09-06 14:49:15 +0900

    Add find_game cloud function (#96)

--- st-cloud/main.py
+++ st-cloud/main.py
@@ -23,7 +23,7 @@ def login(request):
 
     # Get all users matching the requested username
     matching_users_query = users.where('username', '==', username)
-    matching_users = [x for x in matching_users_query.stream()]
+    matching_users = list(matching_users_query.stream())
     user: firestore.DocumentReference = None
 
     if len(matching_users) == 1:
@@ -44,3 +44,41 @@ def login(request):
                  'token_set_time': firestore.SERVER_TIMESTAMP})
 
     return json.dumps({'token': token})
+
+
+def find_game(request):
+    """
+    Find a game for a user.
+    If no other players are waiting, places the player into the waiting queue.
+    """
+    request_json = request.json
+    if 'token' not in request_json:
+        raise RuntimeError("No token provided")
+    token = request.json['token']
+
+    users = db.collection('users')
+
+    matching_users_query = users.where('token', '==', token)
+    matching_users = list(matching_users_query.stream())
+
+    if not matching_users:
+        raise RuntimeError("Invalid token")
+    if len(matching_users) > 1:
+        raise RuntimeError("Duplicate tokens in database, request new token")
+
+    user = matching_users[0]
+
+    waiting_users_query = users.where('status', '==', 'waiting_for_game')
+    waiting_users = [x for x in waiting_users_query.stream()
+                     if x.id != user.id]
+
+    if not waiting_users:
+        users.document(user.id).update({'status': 'waiting_for_game'})
+        return json.dumps({'status': 'waiting_for_game'})
+
+    batch = db.batch()
+    batch.update(users.document(user.id), {'status': 'in_game'})
+    batch.update(users.document(waiting_users[0].id), {'status': 'in_game'})
+    batch.commit()
+
+    return json.dumps({'status': 'in_game'})
