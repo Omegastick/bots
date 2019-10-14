9db4fe1ebbb5793490db29bbc2227b8b777b7678
Omegastick <omegastick@hotmail.co.uk>
2019-10-14 15:34:15 +0900

    Allow for different sized width and height on spring mesh (#102)

--- src/misc/spring_mesh.cpp
+++ src/misc/spring_mesh.cpp
@@ -1,55 +1,43 @@
 #include <cstring>
 
+#include <doctest.h>
 #include <glm/glm.hpp>
-#include <spdlog/spdlog.h>
 
 #include "spring_mesh.h"
 
 namespace SingularityTrainer
 {
-SpringMesh::SpringMesh(int vertices_per_side,
-                       float side_size,
-                       glm::vec2 center,
+SpringMesh::SpringMesh(int width,
+                       int height,
                        float damping,
                        float friction,
                        float stiffness,
                        float elasticity)
-    : vertices_per_side(vertices_per_side),
-      no_vertices(vertices_per_side * vertices_per_side),
-      side_size(side_size),
-      spring_length(side_size / vertices_per_side),
+    : width(width),
+      height(height),
+      no_vertices(width * height),
       damping(damping),
       friction(friction),
       stiffness(stiffness),
       elasticity(elasticity),
       accelerations(no_vertices, {0, 0, 0}),
       offsets(no_vertices, {0, 0, 0}),
-      velocities(no_vertices, {0, 0, 0})
-{
-    positions.reserve(no_vertices);
-    const float half_size = side_size / 2;
-    for (float i = -half_size; i < half_size; i += spring_length)
-    {
-        for (float j = -half_size; j < half_size; j += spring_length)
-        {
-            positions.push_back(glm::vec3{i, j, 0} + glm::vec3{center.x, center.y, 0});
-        }
-    }
-}
+      velocities(no_vertices, {0, 0, 0}) {}
 
 void SpringMesh::apply_explosive_force(glm::vec2 position, float size)
 {
-    for (int row = 0; row < vertices_per_side; ++row)
+    int index = 0;
+    for (int row = 0; row < height; ++row)
     {
-        for (int column = 0; column < vertices_per_side; ++column)
+        for (int column = 0; column < width; ++column)
         {
-            int index = row * vertices_per_side + column;
-            glm::vec3 vertex_position = positions[index] + offsets[index];
+            glm::vec3 vertex_position = glm::vec3{column, row, 0} + offsets[index];
             float distance = glm::length(vertex_position - glm::vec3{position.x, position.y, 0});
             if (distance < size)
             {
-                accelerations[row * vertices_per_side + column] = glm::vec3{0, 0, 100};
+                accelerations[index] = glm::vec3{0, 0, 100};
             }
+            index++;
         }
     }
 }
@@ -64,12 +52,12 @@ void SpringMesh::apply_spring_forces(const glm::vec3 &position_1,
     glm::vec3 vector = position_1 - position_2;
 
     float vector_length = glm::length(vector);
-    if (vector_length <= spring_length)
+    if (vector_length <= 1)
     {
         return;
     }
 
-    vector = (vector / vector_length) * (vector_length - spring_length);
+    vector = (vector / vector_length) * (vector_length - 1);
     glm::vec3 velocity = velocity_2 - velocity_1;
     glm::vec3 force = stiffness * vector - velocity * damping;
 
@@ -77,14 +65,20 @@ void SpringMesh::apply_spring_forces(const glm::vec3 &position_1,
     acceleration_2 += force;
 }
 
-std::vector<glm::vec2> SpringMesh::get_vertices()
+std::vector<glm::vec2> SpringMesh::get_vertices(float scale_x, float scale_y)
 {
     std::vector<glm::vec2> vertices(no_vertices);
-    for (int i = 0; i < no_vertices; ++i)
+    int index = 0;
+    for (int row = 0; row < height; ++row)
     {
-        vertices[i] = {positions[i].x + offsets[i].x,
-                       positions[i].y + offsets[i].y};
+        for (int column = 0; column < width; ++column)
+        {
+            vertices[index] = {(column + offsets[index].x) / (width - 1) * scale_x,
+                               (row + offsets[index].y) / (height - 1) * scale_y};
+            index++;
+        }
     }
+
     return vertices;
 }
 
@@ -93,10 +87,10 @@ void SpringMesh::update()
     // Update vertex positions
     for (int i = 0; i < no_vertices; ++i)
     {
-        if (i < vertices_per_side ||
-            i > no_vertices - vertices_per_side ||
-            i % vertices_per_side == 0 ||
-            i % vertices_per_side == vertices_per_side - 1)
+        if (i < width ||
+            i > no_vertices - width ||
+            i % width == 0 ||
+            i % width == width - 1)
         {
             accelerations[i] = {0, 0, 0};
         }
@@ -115,35 +109,74 @@ void SpringMesh::update()
     ::memset(accelerations.data(), 0.f, accelerations.size() * sizeof(accelerations[0]));
 
     // Apply springs horizontally
-    for (int row = 0; row < vertices_per_side; ++row)
+    int index_1 = 0;
+    for (int row = 0; row < height; ++row)
     {
-        for (int column = 0; column < vertices_per_side - 1; ++column)
+        for (int column = 0; column < width - 1; ++column)
         {
-            int index_1 = row * vertices_per_side + column;
             int index_2 = index_1 + 1;
-            apply_spring_forces(positions[index_1] + offsets[index_1],
-                                positions[index_2] + offsets[index_2],
+            apply_spring_forces(glm::vec3{column, row, 0} + offsets[index_1],
+                                glm::vec3{column + 1, row, 0} + offsets[index_2],
                                 velocities[index_1],
                                 velocities[index_2],
                                 accelerations[index_1],
                                 accelerations[index_2]);
+            index_1++;
         }
     }
 
     // Apply springs vertically
-    for (int column = 0; column < vertices_per_side; ++column)
+    index_1 = 0;
+    for (int column = 0; column < width; ++column)
     {
-        for (int row = 0; row < vertices_per_side - 1; ++row)
+        for (int row = 0; row < height - 1; ++row)
         {
-            int index_1 = row * vertices_per_side + column;
-            int index_2 = index_1 + vertices_per_side;
-            apply_spring_forces(positions[index_1] + offsets[index_1],
-                                positions[index_2] + offsets[index_2],
+            int index_2 = index_1 + width;
+            apply_spring_forces(glm::vec3{column, row, 0} + offsets[index_1],
+                                glm::vec3{column, row + 1, 0} + offsets[index_2],
                                 velocities[index_1],
                                 velocities[index_2],
                                 accelerations[index_1],
                                 accelerations[index_2]);
+            index_1++;
         }
+    }
+}
+
+TEST_CASE("SpringMesh")
+{
+    SpringMesh spring_mesh(3, 4);
+
+    SUBCASE("Number of vertices returned is equal to width * height")
+    {
+        auto vertices = spring_mesh.get_vertices(1, 1);
+
+        CHECK(vertices.size() == 12);
+    }
+
+    SUBCASE("Number of offsets returned is equal to width * height")
+    {
+        auto &offsets = spring_mesh.get_offsets();
+
+        CHECK(offsets.size() == 12);
+    }
+
+    SUBCASE("Small radius explosive force moves correct vertex")
+    {
+        spring_mesh.apply_explosive_force({1, 1}, 0.1);
+        spring_mesh.update();
+
+        auto &offsets = spring_mesh.get_offsets();
+
+        CHECK(offsets[4].z > 1);
+    }
+
+    SUBCASE("Vertices are scaled correctly")
+    {
+        auto vertices = spring_mesh.get_vertices(5, 7);
+
+        CHECK(vertices[0] == glm::vec2{0, 0});
+        CHECK(vertices[11] == glm::vec2(5, 7));
     }
 }
 }
