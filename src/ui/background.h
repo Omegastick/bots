bce5183d4e45ce7f21b97bf31e8ec99d0cdb0228
Omegastick <omegastick@hotmail.co.uk>
2019-12-13 17:38:43 +0900

    Add background to main menu (#168)

---
+++ src/ui/background.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/vec2.hpp>
+
+namespace SingularityTrainer
+{
+class Random;
+class Renderer;
+
+class Background
+{
+  private:
+    struct Node
+    {
+        glm::vec2 position;
+        glm::vec2 drift;
+        Node *neighbors[6] = {nullptr};
+    };
+
+    double delta_time;
+    std::vector<std::size_t> neighbor_indices;
+    std::vector<Node> nodes;
+    Random &rng;
+
+  public:
+    Background(Random &rng);
+
+    void draw(Renderer &renderer) const;
+    void recalculate_neighbors(Node &node);
+    void update(double delta_time);
+};
+}
