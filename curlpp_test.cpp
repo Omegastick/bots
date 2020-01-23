75ee6489406652b1600caae6b93b0bb786378605
Omegastick <omegastick@hotmail.co.uk>
2020-01-23 16:15:14 +0900

    curlpp_test 2

--- curlpp_test.cpp
+++ curlpp_test.cpp
@@ -9,8 +9,9 @@ int main(int /*argc*/, char */*argv*/[]) {
     cURLpp::initialize();
     curlpp::Easy request;
     request.setOpt(new curlpp::options::Url("http://asd.com"));
-    std::list<std::string> headers{"asd", "sdf"};
-    request.setOpt(new curlpp::options::HttpHeader(headers));
+    std::list<std::string> header; 
+    header.push_back("Content-Type: application/octet-stream"); 
+    request.setOpt(new curlpp::options::HttpHeader(header));
     request.perform();
     cURLpp::terminate();
 }
