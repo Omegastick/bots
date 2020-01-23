0cfcdcc8fad5a233c829f70b0c8e6d5ff8610326
Omegastick <omegastick@hotmail.co.uk>
2020-01-23 16:16:55 +0900

    Curlpp test 3

--- curlpp_test.cpp
+++ curlpp_test.cpp
@@ -5,7 +5,7 @@
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
-int main(int /*argc*/, char */*argv*/[]) {
+int main(int /*argc*/, char * /*argv*/ []) {
     cURLpp::initialize();
     curlpp::Easy request;
     request.setOpt(new curlpp::options::Url("http://asd.com"));
